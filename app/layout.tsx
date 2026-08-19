37065e9a684869b36d1331385b3f48c71905b6b4
andredwi93 <andredwi93@gmail.com>
2026-08-19 14:47:07 +0700

    add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -57,6 +58,7 @@ export default function RootLayout({
       <body>
         <NextTopLoader color="#1FB571" />
         <TooltipProvider>{children}</TooltipProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
